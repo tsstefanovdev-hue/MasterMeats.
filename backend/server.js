80fd195dca48b62c7a79472f477f0c7487492f17
tsstefanovdev-hue <tsstefanovdev@gmail.com>
2025-10-26 17:46:06 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ import cartRoutes from "./routes/cart.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import paymentsRoutes from "./routes/payments.routes.js";
 import orderRoutes from "./routes/orders.routes.js";
-import analyticsRoutes from "./routes/analytics.routes.js";
 import localeRoutes from "./routes/locale.routes.js";
 
 import { connectDB } from "./lib/db.js";
@@ -30,10 +29,10 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/payments", paymentsRoutes);
 app.use("/api/orders", orderRoutes);
-app.use("/api/analytics", analyticsRoutes);
 app.use("/api/locales", localeRoutes);
 
 app.listen(PORT, () => {
 	console.log("Server is running on http://localhost:" + PORT);
 	connectDB();
+
 });
